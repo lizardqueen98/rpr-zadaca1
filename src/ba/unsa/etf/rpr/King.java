b9faa20c590ef62d05d9100ea059af97a5d060c2
lizardqueen98 <nadija.b@gmail.com>
2018-11-07 17:36:09 +0100

    pokusaj 1

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -9,10 +9,38 @@ public class King extends ChessPiece {
     @Override
     public void move(String position) throws IllegalChessMoveException{
         new King(position,this.getColor());
-        int indeks1 = slova.indexOf(this.getPosition().charAt(0));
+        int indeks1 = slova.indexOf(toLowerCase(this.getPosition().charAt(0)));
         int indeks2 = brojevi.indexOf(this.getPosition().charAt(1));
-        if(position.charAt(0)==slova.charAt(indeks1) || position.charAt(0)==slova.charAt(indeks1+1) || position.charAt(0)==slova.charAt(indeks1-1)){
-            if(position.charAt(1)==slova.charAt(indeks2) || position.charAt(1)==slova.charAt(indeks2+1) || position.charAt(1)==slova.charAt(indeks2-1)){
+        if(toLowerCase(position.charAt(0))==slova.charAt(indeks1)){
+            if(position.charAt(1)==brojevi.charAt(indeks2)){
+                this.setPosition(position);
+            }
+            else if(indeks2+1<=7 && position.charAt(1)==brojevi.charAt(indeks2+1)){
+                this.setPosition(position);
+            }
+            else if(indeks2-1>=0 && position.charAt(1)==brojevi.charAt(indeks2-1)) {
+                this.setPosition(position);
+            }
+        }
+        else if(indeks1+1<=7 && toLowerCase(position.charAt(0))==slova.charAt(indeks1+1)){
+            if(position.charAt(1)==brojevi.charAt(indeks2)){
+                this.setPosition(position);
+            }
+            else if(indeks2+1<=7 && position.charAt(1)==brojevi.charAt(indeks2+1)){
+                this.setPosition(position);
+            }
+            else if(indeks2-1>=0 && position.charAt(1)==brojevi.charAt(indeks2-1)){
+                this.setPosition(position);
+            }
+        }
+        else if(indeks1-1>=0 && toLowerCase(position.charAt(0))==slova.charAt(indeks1-1)){
+            if(position.charAt(1)==brojevi.charAt(indeks2)){
+                this.setPosition(position);
+            }
+            else if(indeks2+1<=7 && position.charAt(1)==brojevi.charAt(indeks2+1)){
+                this.setPosition(position);
+            }
+            else if(indeks2-1>=0 && position.charAt(1)==brojevi.charAt(indeks2-1)){
                 this.setPosition(position);
             }
         }
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -2,6 +2,12 @@ package ba.unsa.etf.rpr;
 
 public class Main {
      public static void main(String[] args) {
+         King k = new King("E1", ChessPiece.Color.WHITE);
+         try {
+             k.move("D2");
+         }
+         catch(Exception e){
 
+         }
     }
 }
