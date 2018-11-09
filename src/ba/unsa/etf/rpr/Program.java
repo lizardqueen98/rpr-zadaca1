8fd1a86f27af942db1062048e19dffb8058e5ca3
lizardqueen98 <nadija.b@gmail.com>
2018-11-09 19:45:54 +0100

    pokusaj 1

--- src/ba/unsa/etf/rpr/Program.java
+++ src/ba/unsa/etf/rpr/Program.java
@@ -14,59 +14,58 @@ public class Program {
               System.out.println("White move: ");
               whiteMove = ulaz.nextLine();
               if (whiteMove.equals("X")) break;
+              try {
+                  if (whiteMove.length() == 2) {
+                      b.move(Pawn.class, ChessPiece.Color.WHITE, whiteMove);
+                  } else if (whiteMove.length() == 3) {
+                      switch (whiteMove.charAt(0)) {
+                          case 'K':
+                              b.move(King.class, ChessPiece.Color.WHITE, whiteMove.substring(1));
+                              break;
+                          case 'Q':
+                              b.move(Queen.class, ChessPiece.Color.WHITE, whiteMove.substring(1));
+                              break;
+                          case 'R':
+                              b.move(Rook.class, ChessPiece.Color.WHITE, whiteMove.substring(1));
+                              break;
+                          case 'B':
+                              b.move(Bishop.class, ChessPiece.Color.WHITE, whiteMove.substring(1));
+                              break;
+                          case 'N':
+                              b.move(Knight.class, ChessPiece.Color.WHITE, whiteMove.substring(1));
+                              break;
+
+                      }
+                  }
+                  if (b.isCheck(ChessPiece.Color.WHITE)) System.out.println("Check!!!");
+              } catch (Exception e) {
+                  System.out.println("Illegal move.\nUnesite ponovo.");
+                  continue;
+              }
           }
           else whiteMoved=false;
           System.out.println("Black move: ");
           blackMove = ulaz.nextLine();
           if(blackMove.equals("X")) break;
           try {
-              if (whiteMove.length() == 2) {
-                  b.move(Pawn.class, ChessPiece.Color.WHITE, whiteMove);
-              } else if (whiteMove.length() == 3) {
-                  switch (whiteMove.charAt(0)) {
-                      case 'K':
-                          b.move(King.class, ChessPiece.Color.WHITE, whiteMove);
-                          break;
-                      case 'Q':
-                          b.move(Queen.class, ChessPiece.Color.WHITE, whiteMove);
-                          break;
-                      case 'R':
-                          b.move(Rook.class, ChessPiece.Color.WHITE, whiteMove);
-                          break;
-                      case 'B':
-                          b.move(Bishop.class, ChessPiece.Color.WHITE, whiteMove);
-                          break;
-                      case 'N':
-                          b.move(Knight.class, ChessPiece.Color.WHITE, whiteMove);
-                          break;
-
-                  }
-              }
-              if(b.isCheck(ChessPiece.Color.WHITE)) System.out.println("Check!!!");
-          }
-          catch(Exception e){
-              System.out.println("Illegal move.\nUnesite ponovo.");
-              continue;
-          }
-          try {
               if (blackMove.length() == 2) {
                   b.move(Pawn.class, ChessPiece.Color.BLACK, blackMove);
               } else if (blackMove.length() == 3) {
                   switch (blackMove.charAt(0)) {
                       case 'K':
-                          b.move(King.class, ChessPiece.Color.BLACK, blackMove);
+                          b.move(King.class, ChessPiece.Color.BLACK, blackMove.substring(1));
                           break;
                       case 'Q':
-                          b.move(Queen.class, ChessPiece.Color.BLACK, blackMove);
+                          b.move(Queen.class, ChessPiece.Color.BLACK, blackMove.substring(1));
                           break;
                       case 'R':
-                          b.move(Rook.class, ChessPiece.Color.BLACK, blackMove);
+                          b.move(Rook.class, ChessPiece.Color.BLACK, blackMove.substring(1));
                           break;
                       case 'B':
-                          b.move(Bishop.class, ChessPiece.Color.BLACK, blackMove);
+                          b.move(Bishop.class, ChessPiece.Color.BLACK, blackMove.substring(1));
                           break;
                       case 'N':
-                          b.move(Knight.class, ChessPiece.Color.BLACK, blackMove);
+                          b.move(Knight.class, ChessPiece.Color.BLACK, blackMove.substring(1));
                           break;
 
                   }
